a1223cdc95f5a3fc2fdf7d32e222e4134409ba9f
Arkalithe <arkalithe@gmail.com>
2024-09-17 16:55:12 +0200

    Table lodging

---
+++ src/main/java/fr/hbvt/springboot/entity/Lodging.java
@@ -0,0 +1,39 @@
+package fr.hbvt.springboot.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Data
+@Table(
+        indexes = {@Index(columnList = "slug")}
+)
+public class Lodging {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int capacity;
+
+    @Column(nullable = false)
+    private boolean isAccessible;
+
+    @Column(nullable = false)
+    private int nightPrice;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false)
+    private String slug;
+
+}
